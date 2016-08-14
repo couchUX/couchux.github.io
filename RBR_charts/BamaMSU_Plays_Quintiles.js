9adf52cb782f11b0571f432cb7123494a56edbeb
Alex Couch <couch.ux@gmail.com>
2016-08-14 11:56:53 -0700

    cleaned up some js
    Consolidated quarters stuff using functions

--- RBR_charts/BamaMSU_Plays_Quintiles.js
+++ RBR_charts/BamaMSU_Plays_Quintiles.js
@@ -7,8 +7,8 @@ playsChart(csv_url);
 
 /* tie chart colors to team names */
 var teamList = {
-  "team1": "Alabama",
-  "team2": "Michigan State"
+  team1: "Alabama",
+  team2: "Michigan State"
 }
 var teamColors = {
   "Alabama": "#C31C45",
@@ -34,7 +34,7 @@ function csv_response(error, data) {
 function render_chart() {
 
 var layout = {
-  barMarginTop: 16,
+  barMarginTop: 20,
   nsOpacity: 0.3,
 }
 var containerWidth = document.getElementById("plays-charts-container").offsetWidth
@@ -42,8 +42,8 @@ var containerWidth = document.getElementById("plays-charts-container").offsetWid
     barWidth = chartWidth / 20
     maxIndex = d3.max(playsData, function(d,i) { return (i + 1) });
     totalPlaysMax = d3.max(playsData, function(d,i) { return d.Total_plays_max })
-    chartHeight = totalPlaysMax * barWidth * 2
-    gridHeight = totalPlaysMax * barWidth + layout.barMarginTop
+    barMaxHeight = totalPlaysMax * barWidth
+    chartHeight = (barMaxHeight + layout.barMarginTop) * 2
     playsChartName = "plays-chart"
 
     selectChartClass = function() {
@@ -58,22 +58,22 @@ var containerWidth = document.getElementById("plays-charts-container").offsetWid
     barColor = function(d,i) {
         return teamColor(d.Team)
     }
-    team1_filter = function(d,i) {
+    team1_fil = function(d,i) {
         return d.Team == teamList.team1
     }
-    team2_filter = function(d,i) {
+    team2_fil = function(d,i) {
         return d.Team == teamList.team2
     }
-    q1_filter = function(d,i) {
+    q1_fil = function(d,i) {
         return (d.Quarter_quintile > 1 && d.Quarter_quintile < 2)
     }
-    q2_filter = function(d,i) {
+    q2_fil = function(d,i) {
         return (d.Quarter_quintile > 2 && d.Quarter_quintile < 3)
     }
-    q3_filter = function(d,i) {
+    q3_fil = function(d,i) {
         return (d.Quarter_quintile > 3 && d.Quarter_quintile < 4)
     }
-    q4_filter = function(d,i) {
+    q4_fil = function(d,i) {
         return d.Quarter_quintile > 4
     }
     barHeight = function(d,i) {
@@ -93,25 +93,36 @@ var containerWidth = document.getElementById("plays-charts-container").offsetWid
           return
         }
     }
-    groupX = function(q) {
+    qX = function(q) {
       return "translate("
              + chartWidth / 4 * (q - 1)
              + ",0)"
+    }
+    topGroupY = function(q) {
+      return "translate(0,"
+             + layout.barMarginTop
+             + ")"
+    }
+    bottomGroupY = function(q) {
+      return "translate(0,"
+             + (layout.barMarginTop + barMaxHeight)
+             + ")"
     }
     barX = function(d,i) {
       return i / (maxIndex / 2) * chartWidth
     }
     topBarY = function(d,i) {
-      return chartHeight / 2 + layout.barMarginTop - d.Total_plays * barWidth
+      return barMaxHeight + layout.barMarginTop - d.Total_plays * barWidth
     }
     topBarY_S = function(d,i) {
-      return chartHeight / 2 + layout.barMarginTop - d.S_plays * barWidth
+      return barMaxHeight + layout.barMarginTop - d.S_plays * barWidth
     }
     topBarY_X = function(d,i) {
-      return chartHeight / 2 + layout.barMarginTop - d.X_plays * barWidth
+      return barMaxHeight + layout.barMarginTop - d.X_plays * barWidth
     }
-    bottomBarY_all = function(d,i) {
-      return chartHeight / 2 + layout.barMarginTop
+    bottomBarY_all = 0
+    q_to_class = function(q) {
+      return "q" + q
     }
 
 /* actually building the chart in d3 */
@@ -128,182 +139,63 @@ var chart = d3.select("#plays-charts-container")
       .attr("class","top-group")
     bottomGroup = svg.append("g")
       .attr("class","bottom-group")
+      .attr("transform",bottomGroupY)
     topGrid = topGroup.append("g")
       .attr("class","top-grid")
     bottomGrid = bottomGroup.append("g")
       .attr("class","bottom-grid")
 
-renderTop = function(q, q_filter, q_class) {
-      topBarsNS(q, q_filter, q_class)
-      topBarsS(q, q_filter, q_class)
-      topBarsX(q, q_filter, q_class)
-      topGridV(q, q_filter, q_class)
-}
-renderBottom = function(q, q_filter, q_class) {
-      bottomBarsNS(q, q_filter, q_class)
-      bottomBarsS(q, q_filter, q_class)
-      bottomBarsX(q, q_filter, q_class)
-      bottomGridV(q, q_filter, q_class)
-}
-renderTop(1, q1_filter, "top-bars .q1")
-renderTop(2, q2_filter, "top-bars .q2")
-renderTop(3, q3_filter, "top-bars .q3")
-renderTop(4, q4_filter, "top-bars .q4")
-renderBottom(1, q1_filter, "bottom-bars .q1")
-renderBottom(2, q2_filter, "bottom-bars .q2")
-renderBottom(3, q3_filter, "bottom-bars .q3")
-renderBottom(4, q4_filter, "bottom-bars .q4")
+renderQuarter(1,q1_fil)
+renderQuarter(2,q2_fil)
+renderQuarter(3,q3_fil)
+renderQuarter(4,q4_fil)
 
+var gridAttr = d3.selectAll(".grid")
+      .attr("fill","none")
+      .style("stroke","white")
+      .style("stroke-width",2)
+
+/*
 renderGridHz()
-
+*/
 
 /* supporting functions for top and bottom bars */
-function topBarsNS(q, q_filter, q_class) {
-      topGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".top-bars")
-      .data(playsData)
-      .enter()
-      .filter(team1_filter)
-      .filter(q_filter)
-      .append("rect")
-      .attr("class","top-bars")
-      .attr("fill",barColor)
-      .attr("opacity",layout.nsOpacity)
-      .attr("width",barWidth)
-      .attr("height",barHeight)
-      .attr("x",barX)
-      .attr("y",topBarY)
+function renderQuarter(q, q_fil) {
+  renderBars(team1_fil, topGroup, q, q_fil, ".top-bars", "top-bars ns", layout.nsOpacity, barHeight, topBarY)
+  renderBars(team1_fil, topGroup, q, q_fil, ".top-bars", "top-bars s", 1, barHeight_S, topBarY_S)
+  renderBars(team1_fil, topGroup, q, q_fil, ".top-bars", "top-bars expBars", 1, barHeight_X, topBarY_X)
+  renderBars(team1_fil, topGroup, q, q_fil, ".top-bars", "top-bars grid", 1, barHeight, topBarY)
+  renderBars(team2_fil, bottomGroup, q, q_fil, ".bottom-bars", "bottom-bars ns", layout.nsOpacity, barHeight, bottomBarY_all)
+  renderBars(team2_fil, bottomGroup, q, q_fil, ".bottom-bars", "bottom-bars s", 1, barHeight_S, bottomBarY_all)
+  renderBars(team2_fil, bottomGroup, q, q_fil, ".bottom-bars", "bottom-bars expBars", 1, barHeight_X, bottomBarY_all)
+  renderBars(team2_fil, bottomGroup, q, q_fil, ".bottom-bars", "bottom-bars grid", 1, barHeight, bottomBarY_all)
 }
 
-function topBarsS(q, q_filter, q_class) {
-      topGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".top-bars")
+function renderBars(team_fil, top_bottom, q, q_fil, bar_cl_sel, bar_cl_set, bar_o, bar_height, bar_y) {
+      top_bottom.append("g")
+      .attr("class",q_to_class(q))
+      .attr("transform",qX(q))
+      .selectAll(bar_cl_sel)
       .data(playsData)
       .enter()
-      .filter(team1_filter)
-      .filter(q_filter)
+      .filter(team_fil)
+      .filter(q_fil)
       .append("rect")
-      .attr("class","top-bars success")
+      .attr("class",bar_cl_set)
       .attr("fill",barColor)
+      .attr("opacity",bar_o)
       .attr("width",barWidth)
-      .attr("height",barHeight_S)
+      .attr("height",bar_height)
       .attr("x",barX)
-      .attr("y",topBarY_S)
-}
-function topBarsX(q, q_filter, q_class) {
-      topGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".top-bars")
-      .data(playsData)
-      .enter()
-      .filter(team1_filter)
-      .filter(q_filter)
-      .append("rect")
-      .attr("class","top-bars expBars")
-      .attr("width",barWidth)
-      .attr("height",barHeight_X)
-      .attr("x",barX)
-      .attr("y",topBarY_X)
-}
-function bottomBarsNS(q, q_filter, q_class) {
-      bottomGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".top-bars")
-      .data(playsData)
-      .enter()
-      .filter(team2_filter)
-      .filter(q_filter)
-      .append("rect")
-      .attr("class","top-bars")
-      .attr("fill",barColor)
-      .attr("opacity",layout.nsOpacity)
-      .attr("width",barWidth)
-      .attr("height",barHeight)
-      .attr("x",barX)
-      .attr("y",bottomBarY_all)
-}
-function bottomBarsS(q, q_filter, q_class) {
-      bottomGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".top-bars")
-      .data(playsData)
-      .enter()
-      .filter(team2_filter)
-      .filter(q_filter)
-      .append("rect")
-      .attr("class","top-bars success")
-      .attr("fill",barColor)
-      .attr("width",barWidth)
-      .attr("height",barHeight_S)
-      .attr("x",barX)
-      .attr("y",bottomBarY_all)
-}
-function bottomBarsX(q, q_filter, q_class) {
-      bottomGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".top-bars")
-      .data(playsData)
-      .enter()
-      .filter(team2_filter)
-      .filter(q_filter)
-      .append("rect")
-      .attr("class","top-bars expBars")
-      .attr("width",barWidth)
-      .attr("height",barHeight_X)
-      .attr("x",barX)
-      .attr("y",bottomBarY_all)
-}
-function topGridV(q, q_filter, q_class) {
-      topGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".gridLine")
-      .data(playsData)
-      .enter()
-      .filter(team1_filter)
-      .filter(q_filter)
-      .append("rect")
-      .attr("class","gridLine")
-      .attr("fill","none")
-      .attr("width",barWidth)
-      .attr("height",gridHeight)
-      .attr("x",barX)
-      .style("stroke","white")
-      .style("stroke-width",2)
-}
-function bottomGridV(q, q_filter, q_class) {
-      bottomGroup.append("g")
-      .attr("class",q_class)
-      .attr("transform",groupX(q))
-      .selectAll(".gridLine")
-      .data(playsData)
-      .enter()
-      .filter(team1_filter)
-      .filter(q_filter)
-      .append("rect")
-      .attr("class","gridLine")
-      .attr("fill","none")
-      .attr("width",barWidth)
-      .attr("height",gridHeight)
-      .attr("x",barX)
-      .attr("y",bottomBarY_all)
-      .style("stroke","white")
-      .style("stroke-width",2)
+      .attr("y",bar_y)
 }
 
+/*
 function renderGridHz(top_bottom, grid_num) { top_bottom.append("rect")
       .attr("width",chartWidth)
       .attr("height",barWidth)
       .attr("fill","none")
       .attr("y",layout.barMarginTop)
 }
-
+*/
 } //end of building chart in d3
