112026e740009ffef3b0058d3652abbb570c32e2
Alex Couch <couch.ux@gmail.com>
2016-09-07 09:12:26 -0700

    adding js for prompt

--- RBR_charts/backup_images.js
+++ RBR_charts/backup_images.js
@@ -1,8 +1,10 @@
 /* set a backup image option */
 var backupButtons = document.getElementById("backup-img-button");
 var backupImgs = document.getElementById("backup-img");
+var backupImgPrompt = document.getElementById("backup-img-prompt");
 
 backupButtons.onclick = function() {
     backupImgs.style.display = "inherit";
     backupButtons.style.display = "none";
+    backupImgPrompt.style.display = "none";
 }
