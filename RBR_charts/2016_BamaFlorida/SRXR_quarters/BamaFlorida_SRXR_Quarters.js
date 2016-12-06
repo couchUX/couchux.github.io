78b97b4178be26b255b618cb2a1dc3bf83850778
Alex Couch <couch.ux@gmail.com>
2016-12-05 19:12:54 -0800

    Changed UF bars color

--- RBR_charts/2016_BamaFlorida/SRXR_quarters/BamaFlorida_SRXR_Quarters.js
+++ RBR_charts/2016_BamaFlorida/SRXR_quarters/BamaFlorida_SRXR_Quarters.js
@@ -45,7 +45,7 @@ function render_SRXRquarters_chart(which_team, what_order) {
 /* tie chart colors to team names */
 var teamColors = {
   "Alabama": "#C31C45",
-  "Florida": "#0021A5"
+  "Florida": "#F26E4D"
 }
     gameColor = "#5C5C5C"
 
