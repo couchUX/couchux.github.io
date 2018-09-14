3417a7fec02eaed8ca3694bf7c22c1d9d1faada4
couchUX <couch.ux@gmail.com>
2018-09-14 14:08:28 -0700

    added flyout v2 folder

---
+++ flyout/v2/flyout.js
@@ -0,0 +1,19 @@
+var toggle = function () {
+
+    $(this).parent().children().toggle();
+    $(this).toggle();
+
+};
+
+var caretSwitch = function () {
+    $(this).toggleClass("up");
+}
+
+var flyoutHide = function () {
+    $(".flyout").toggleClass("flyout--hide");
+}
+
+$(".tog").click(toggle);
+$(".tog").not(".link").click(caretSwitch);
+$(".tog").each(toggle);
+$(".nav--jobs").click(flyoutHide).click(caretSwitch);
