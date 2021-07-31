abff866e15d8d58617640d58e486b42ea74b79af
Alex Couch <amcouch@gmail.com>
2021-07-31 12:12:01 -0700

    Testing out a bunch of stuff for new charting flow

---
+++ RBR_charts/cfb-charts/test-files/rootdata.js
@@ -0,0 +1,28 @@
+const rootData = [
+  {
+    x: "Alabama",
+    successRate: 0.566,
+    explosivenessRate: 0.181,
+    // "colorMain": "rgba(244, 37, 37, 0.8)",
+    // "colorDark": "rgba(128, 0, 0, 0.8)",
+    // "colorLight": "rgba(255, 230, 230, 0.8)"
+  },
+  {
+    x: "Florida",
+    successRate: 0.485,
+    explosivenessRate: 0.167,
+    // "colorMain": "rgba(244, 97, 37, 0.8)",
+    // "colorDark": "rgba(128, 37, 0, 0.8)",
+    // "colorLight": "rgba(255, 237, 230, 0.8)"
+  }
+]
+
+// const successColors = [
+//     "rgba(244, 37, 37, 0.8)",
+//     "rgba(244, 97, 37, 0.8)"
+// ]
+
+// const explosiveColors = [
+//   "rgba(128, 0, 0, 0.8)",
+//   "rgba(128, 37, 0, 0.8)"
+// ]
---
+++ RBR_charts/cfb-charts/test-files/simpledata.js
@@ -0,0 +1,1 @@
+const simpleData = [1,2,3]
